03365062336463f3eb2ce6d021855bea45bf7ab7
Verdagon <verdagon@gmail.com>
2020-08-02 21:25:09 -0700

    Made explosions a bit cooler, fixed turn_delay

--- benchmarks/BenchmarkRL/rust/src/explodey.rs
+++ benchmarks/BenchmarkRL/rust/src/explodey.rs
@@ -24,16 +24,27 @@ impl IUnitComponent for ExplodeyUnitComponent {
         let self_unit = &game.units[self_unit_index];
         // The visible area is a good approximation of where a blast
         // would go.
-        let blasted_locs =
-            level.get_locations_within_sight(self_unit.loc, false, BLAST_RANGE);
+        let blasted_locs = level.get_locations_within_sight(self_unit.loc, false, BLAST_RANGE);
+        let mut walkable_blasted_locs = Vec::new();
+        for blasted_loc in blasted_locs {
+            if level.loc_is_walkable(blasted_loc, false) {
+                walkable_blasted_locs.push(blasted_loc);
+            }
+        }
+        let center_loc = self_unit.loc;
 
         // We can't modify the game from in here because we borrowed the game
         // immutably.
         // So, we return a lambda that will modify the game for us.
         return Box::new(move |rand, game| {
-            for &blasted_loc in &blasted_locs {
+            for &blasted_loc in &walkable_blasted_locs {
+                // Lets make it burn longer if it was close.
+                let distance_from_center_100 = center_loc.diagonal_manhattan_distance_100(blasted_loc);
+                // 0 means on the edge, 100 means at center.
+                let closeness_to_center_100 = 100 - distance_from_center_100 * 100 / BLAST_RANGE;
+                let num_turns_to_burn = closeness_to_center_100 as u32 / 10 + rand.next() % 10;
+
                 let level = game.get_current_level_mut();
-                let num_turns_to_burn = rand.next() % 30;
                 level.tiles.get_mut(&blasted_loc).expect("").components.insert(
                     Box::new(FireTileComponent::new(num_turns_to_burn)));
                 level.register_acting_tile(blasted_loc);

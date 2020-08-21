93879435e6ac64b63100ef600d622477317af34d
Verdagon <verdagon@gmail.com>
2020-08-21 13:31:20 -0700

    Ready to fix index OOB problem

--- Midas/src/valestd/weaks.c
+++ Midas/src/valestd/weaks.c
@@ -98,7 +98,7 @@ uint64_t __allocWrc() {
   }
 
   resultWrci = wrcTable.firstFree;
-  printf("Allocated wrci %d from free list\n", resultWrci);
+//  printf("Allocated wrci %d from free list\n", resultWrci);
   wrcTable.firstFree = wrcTable.entries[resultWrci];
 
   wrcTable.size++;
@@ -109,7 +109,7 @@ uint64_t __allocWrc() {
 
 int8_t __wrcIsLive(uint64_t wrcIndex) {
   if (wrcIndex >= wrcTable.capacity) {
-    printf("Bad wrci: %d\n", wrcIndex);
+//    printf("Bad wrci: %d\n", wrcIndex);
     assert(0);
   }
   int8_t alive = (wrcTable.entries[wrcIndex] & WRC_LIVE_BIT) != 0;
@@ -119,42 +119,42 @@ int8_t __wrcIsLive(uint64_t wrcIndex) {
 // Warning: can have false positives, where it says something's valid when it's not.
 void __checkWrc(uint64_t wrcIndex) {
   if (wrcIndex >= wrcTable.capacity) {
-    printf("Bad wrci: %d\n", wrcIndex);
+//    printf("Bad wrci: %d\n", wrcIndex);
     assert(0);
   }
 }
 
 void __incrementWrc(uint64_t wrcIndex) {
   if (wrcIndex >= wrcTable.capacity) {
-    printf("Bad wrci: %d\n", wrcIndex);
+//    printf("Bad wrci: %d\n", wrcIndex);
     assert(0);
   }
   wrcTable.entries[wrcIndex]++;
-  printf("incremented %d, now %d\n", wrcIndex, wrcTable.entries[wrcIndex]);
+//  printf("incremented %d, now %d\n", wrcIndex, wrcTable.entries[wrcIndex]);
 }
 
 void __decrementWrc(uint64_t wrcIndex) {
   if (wrcIndex >= wrcTable.capacity) {
-    printf("Bad wrci: %d\n", wrcIndex);
+//    printf("Bad wrci: %d\n", wrcIndex);
     assert(0);
   }
   wrcTable.entries[wrcIndex]--;
-  printf("decremented %d, now %d\n", wrcIndex, wrcTable.entries[wrcIndex]);
+//  printf("decremented %d, now %d\n", wrcIndex, wrcTable.entries[wrcIndex]);
   if (wrcTable.entries[wrcIndex] == 0) {
-    printf("releasing! %d\n", wrcIndex);
+//    printf("releasing! %d\n", wrcIndex);
     releaseWrc(wrcIndex);
   }
 }
 
 void __markWrcDead(uint64_t wrcIndex) {
   if (wrcIndex >= wrcTable.capacity) {
-    printf("Bad wrci: %d\n", wrcIndex);
+//    printf("Bad wrci: %d\n", wrcIndex);
     assert(0);
   }
   wrcTable.entries[wrcIndex] &= ~WRC_LIVE_BIT;
-  printf("marking %d dead!\n", wrcIndex);
+//  printf("marking %d dead!\n", wrcIndex);
   if (wrcTable.entries[wrcIndex] == 0) {
-    printf("releasing! %d\n", wrcIndex);
+//    printf("releasing! %d\n", wrcIndex);
     releaseWrc(wrcIndex);
   }
 }

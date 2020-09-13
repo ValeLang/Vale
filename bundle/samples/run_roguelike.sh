cc8cc30d5448a59af24a6782838c1ab3734ce75b
Verdagon <verdagon@gmail.com>
2020-09-12 21:29:06 -0700

    Reorganized folders and made a release script

--- bundle/samples/run_roguelike.sh
+++
@@ -1,2 +0,0 @@
-python3.8 ../valec.py --gen-heap --region-override resilient-v2 roguelike.vale ../vstl/generics/arrayutils.vale ../vstl/genericvirtuals/hashmap.vale ../vstl/genericvirtuals/opt.vale ../vstl/utils.vale ../vstl/castutils.vale ../vstl/printutils.vale ../vstl/genericvirtuals/optingarraylist.vale
-./a.out

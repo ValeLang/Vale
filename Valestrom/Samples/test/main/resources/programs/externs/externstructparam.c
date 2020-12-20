16a482ed104e8e2c805d82dfdff1bfb228ab1ae6
Evan Ovadia <verdagon@gmail.com>
2020-12-19 20:01:17 -0800

    Export structs working except for RC issues

--- Valestrom/Samples/test/main/resources/programs/externs/externstructparam.c
+++ Valestrom/Samples/test/main/resources/programs/externs/externstructparam.c
@@ -6,9 +6,9 @@
 //  uint64_t ignore_1; // generation. if 0xFF, this is the owning reference.
 //  void* ignore_2; // ptr to obj.
 //} SpaceshipRef;
-//extern int32_t Spaceship_get_a(Spaceship* s);
-//extern int32_t Spaceship_get_b(Spaceship* s);
+extern int64_t spaceshipGetA(SpaceshipRef s);
+extern int64_t spaceshipGetB(SpaceshipRef s);
 
 int64_t sumSpaceshipFields(SpaceshipRef s) {
-  return SpaceshipRef_get_a(s) + SpaceshipRef_get_b(s);
+  return spaceshipGetA(s) + spaceshipGetB(s);
 }

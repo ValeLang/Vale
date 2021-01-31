d986e6f32eb42e2b8d545d3edafa62fe5b921d64
theowatkins <theowatkins1@gmail.com>
2021-01-31 15:08:01 -0500

    removed Catalyst to clean

--- Catalyst/testing/make_vast_files.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-ECHO Compiling test programs...
-SET VALE_COMPILER=C:\Users\theow\Desktop\Thesis\ValeCompiler-0.1.0.0-Win
-
-FOR /f "tokens=*" %%A IN ('dir /b /ad') DO IF NOT EXIST %cd%\%%A\out\main.exe CALL python3 %VALE_COMPILER%\valec.py %cd%\%%A\main.vale --gen-heap --region-override resilient-v3 --output-dir %cd%\%%A\out --elide-checks-for-known-live --print-mem-overhead
-
-del *.obj

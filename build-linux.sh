2852fc96fd6af645079a49d251bb81f4620edf1d
Evan Ovadia <verdagon@gmail.com>
2021-03-03 14:15:31 -0800

    Removed requirement for sudo to run the script

--- build-linux.sh
+++ build-linux.sh
@@ -1,23 +1,22 @@
 # Building the Vale Compiler (Linux)
 
 # This script shows how to build the Vale compiler.
-# This can be used from a Ubuntu VM. It would be run with sudo.
-# However, we don't recommend running scripts from the internet with sudo access,
-# so best just read the file and follow the instructions in the comments.
+# This can be used from a Ubuntu VM.
+# WARNING: There are sudo commands below! Review all sudo commands before running them.
 
 ## Prerequisites
 
 # Install Java
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
-add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-apt update
-apt install adoptopenjdk-11-hotspot # Java 11 / HotSpot VM
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt update
+sudo apt install adoptopenjdk-11-hotspot # Java 11 / HotSpot VM
 
 # Install SBT
-echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
-apt-get update
-apt-get install -y sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install -y sbt
 
 # Install LLVM 11; download the release for your OS
 # from https://github.com/llvm/llvm-project/releases/tag/llvmorg-11.0.1
@@ -25,7 +24,7 @@ curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/cl
 tar xf llvm11.tar.xz
 
 # Install dependencies
-apt install curl git cmake clang-11 zlib1g-dev
+sudo apt install curl git cmake clang-11 zlib1g-dev
 # Add vars to .zshrc (or .bashrc)
 echo 'export LDFLAGS="-L~/llvm11/lib -Wl,-rpath,~/llvm11/lib"' >> ~/.bashrc
 echo 'export CPPFLAGS="-I~/llvm11/include"' >> ~/.bashrc
@@ -65,3 +64,4 @@ cd ../scripts
 cd ../release-unix
 
 zip -r ValeCompiler.zip *
+

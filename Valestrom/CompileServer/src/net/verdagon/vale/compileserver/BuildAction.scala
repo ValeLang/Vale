8fba8428da84b685e3f8e5f6e263c11f878af6f6
Evan Ovadia <verdagon@gmail.com>
2021-05-07 22:41:42 -0700

    Made build scripts support vpst

--- Valestrom/CompileServer/src/net/verdagon/vale/compileserver/BuildAction.scala
+++ Valestrom/CompileServer/src/net/verdagon/vale/compileserver/BuildAction.scala
@@ -15,7 +15,7 @@ class BuildAction extends HttpFunction {
     }
 
     val json =
-      Driver.build(Options(List("in.vale"), Some(""), false, true, false, None, false), List(code)) match {
+      Driver.build(Options(List("in.vale"), Some(""), false, true, false, true, None, false), List(code)) match {
         case Ok(Some(programH)) => jsonifyProgram(programH)
         case Err(error) => {
           response.setStatusCode(400)

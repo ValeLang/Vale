482edca19c255826d04bcf1f400faa10ac878aee
Evan Ovadia <verdagon@gmail.com>
2021-08-02 10:06:38 -0700

    Adding flares

--- Midas/test/testtwinpages.c
+++ Midas/test/testtwinpages.c
@@ -1,8 +1,38 @@
+#ifdef _WIN32
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <signal.h>
+
+#else
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+#endif
+
+
+int pageSize() {
+#ifdef _WIN32
+  DWORD dwPageSize;             // amount of memory to allocate.
+  SYSTEM_INFO sSysInfo;         // useful information about the system
+  GetSystemInfo(&sSysInfo);     // initialize the structure
+  return sSysInfo.dwPageSize;
+#else
+  return getpagesize();
+#endif
+}
+
+
+void SignalHandler(int signal)
+{
+    printf("Memory violation! Signal %d\n", signal);
+}
 
 const char attemptbadwritestr[] = "attemptbadwrite";
 const char noattemptbadwritestr[] = "noattemptbadwrite";
@@ -19,49 +49,80 @@ int main(int argc, char** argv) {
   }
   int attemptBadWrite = strncmp(argv[1], attemptbadwritestr, strlen(attemptbadwritestr)) == 0;
 
+  // Set up the signal to catch the seg fault.
+  // This is nice for windows, which otherwise just silently fails.
+  typedef void (*SignalHandlerPointer)(int);
+  SignalHandlerPointer previousHandler;
+  previousHandler = signal(SIGSEGV , SignalHandler);
 
-  size_t pagesize = getpagesize();
+
+  size_t pagesize = pageSize();
 
   printf("System page size: %zu bytes\n", pagesize);
 
-  char *region = mmap(
-    0,
-    pagesize * 2,
-    0,
-    MAP_ANON | MAP_PRIVATE,
-    0,
-    0
-  );
-
-  if (region == MAP_FAILED) {
+#ifdef _WIN32
+  char* allocationPtr =
+      VirtualAlloc(
+          NULL, pagesize * 2, MEM_RESERVE | MEM_COMMIT, PAGE_NOACCESS);
+  if(allocationPtr == NULL) {
+    _tprintf(TEXT("VirtualAlloc failed. Error: %ld\n"), GetLastError());
+    return 1;
+  }
+#else
+  char *allocationPtr =
+      mmap(0, pagesize * 2, 0, MAP_ANON | MAP_PRIVATE, 0, 0);
+  if (allocationPtr == MAP_FAILED) {
     perror("Could not mmap");
     return 1;
   }
+#endif
 
-  if (mprotect(region, pagesize, PROT_READ | PROT_WRITE)) {
+  printf("Successfully allocated two pages, starting at %p\n", allocationPtr);
+
+#ifdef _WIN32
+  DWORD oldFlags = 0;
+  int virtualProtectSuccess =
+      VirtualProtect(
+          allocationPtr, pagesize, PAGE_READWRITE, &oldFlags);
+  if (!virtualProtectSuccess) {
+    _tprintf(TEXT("VirtualProtect failed. Error: %ld\n"), GetLastError());
+    exit(1);
+  }
+#else
+  if (mprotect(allocationPtr, pagesize, PROT_READ | PROT_WRITE)) {
     perror("Could not mprotect");
     return 1;
   }
+#endif
 
   printf("Writing 42 to the end of the unprotected twin page!\n");
-  *(long long*)&region[pagesize - sizeof(long long)] = 42LL;
+  *(long long*)&allocationPtr[pagesize - sizeof(long long)] = 42LL;
 
   printf("Reading from the end of the unprotected twin page!\n");
-  long long result = *(long long*)&region[pagesize - sizeof(long long)];
+  long long result = *(long long*)&allocationPtr[pagesize - sizeof(long long)];
   printf("Got: %lld\n", result);
 
   if (attemptBadWrite) {
     printf("Writing to the beginning of the protected twin page, should crash!\n");
-    region[pagesize] = 73LL;
+    allocationPtr[pagesize] = 73LL;
   }
 
-  int unmap_result = munmap(region, 1 << 10);
+#ifdef _WIN32
+  int virtualFreeSuccess = VirtualFree(allocationPtr, 0, MEM_RELEASE);
+  if (!virtualFreeSuccess) {
+    _tprintf(TEXT("VirtualFree failed. Error: %ld\n"), GetLastError());
+    exit(1);
+  }
+#else
+  int unmap_result = munmap(allocationPtr, pagesize * 2);
   if (unmap_result != 0) {
     perror("Could not munmap");
     return 1;
   }
+#endif
 
   if (result == 42LL) {
+    printf("Success!\n");
     return 0;
   } else {
     return 1;

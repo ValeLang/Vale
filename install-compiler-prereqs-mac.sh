728e25ec7a2f2c5831164c5e80df2dcf21d04212
Evan Ovadia <verdagon@gmail.com>
2021-08-03 12:14:39 -0700

    Added mac scripts

---
+++ install-compiler-prereqs-mac.sh
@@ -0,0 +1,20 @@
+# This script installs all the prerequisites for building the Vale compiler.
+
+# WARNING: There are sudo commands below! Review all sudo commands before running them.
+
+# Installs Xcode if you haven't already.
+xcode-select --install
+# (press ok)
+
+# Installs brew, like said on https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# (press return)
+
+# Install misc dependencies
+brew install llvm@11 sbt cmake
+
+# Install Java.
+curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.10_9.tar.gz --output adoptopenjdk.tar.gz
+tar xzf adoptopenjdk.tar.gz
+# Add vars to .zshrc (or .bashrc)
+echo 'export PATH=~/jdk-11.0.10+9-jre/Contents/Home/bin:$PATH' >> ~/.zshrc

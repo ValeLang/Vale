f212c2210a5702915ee61c0dc7059fac4dabdab6
Evan Ovadia <verdagon@gmail.com>
2021-08-10 21:01:56 -0700

    Removed testbuild directory

--- Tester/testbuild/addret_assist/vstr_indexOf.h
+++
@@ -1,6 +0,0 @@
-#ifndef VALE_EXPORTS_vstr_indexOf_H_
-#define VALE_EXPORTS_vstr_indexOf_H_
-#include "ValeBuiltins.h"
-extern int32_t vstr_indexOf(ValeStr* param0, int32_t param1, int32_t param2, ValeStr* param3, int32_t param4, int32_t param5);
-extern int32_t vale_abi_vstr_indexOf(ValeStr* param0, int32_t param1, int32_t param2, ValeStr* param3, int32_t param4, int32_t param5);
-#endif

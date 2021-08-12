72d03806bdc1b788bc6771e18714c4de497e03b8
Evan Ovadia <verdagon@gmail.com>
2021-08-11 19:06:30 -0700

    Windows fixes

--- Tester/build.bat
+++ Tester/build.bat
@@ -1,1 +1,1 @@
-..\Driver\build\valec build tester=src stdlib=~/stdlib/src --output_dir build -o tester
+..\Driver\build\valec build tester=src stdlib=..\stdlib\src --output_dir build -o tester --builtins_dir_override ..\Midas\src\builtins --valestrom_dir_override ..\Valestrom --midas_dir_override ..\Midas\build

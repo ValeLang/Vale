e05645654789d88e16c19b5eb33debeda6ce4531
Evan Ovadia <verdagon@gmail.com>
2021-08-13 14:32:04 -0700

    Bootstrapping fixes

--- scripts/package-unix.sh
+++ scripts/package-unix.sh
@@ -5,8 +5,6 @@ cp ../Valestrom/Valestrom.jar ../release-unix
 
 cp -r ../Valestrom/Tests/test/main/resources/programs ../release-unix/samples
 cp -r ../Midas/src/builtins ../release-unix/builtins
-cp -r ../Midas/vstl ../release-unix/vstl
-cp ../Midas/valec.py ../release-unix/valec.py
 cp releaseREADME.txt ../release-unix/README.txt
 cp valec-* ../release-unix
 cp ../Midas/build/midas ../release-unix/midas

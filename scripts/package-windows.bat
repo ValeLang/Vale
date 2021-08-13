9eaf0996a39dae355b79bfbe4283180c2cd474de
Evan Ovadia <verdagon@gmail.com>
2021-08-13 14:27:20 -0700

    Bootstrapping fixes

--- scripts/package-windows.bat
+++ scripts/package-windows.bat
@@ -6,10 +6,10 @@ copy ..\Valestrom\Valestrom.jar ..\release-windows\Valestrom.jar
 echo d | xcopy /s /e /y ..\Valestrom\Tests\test\main\resources\programs ..\release-windows\samples
 echo d | xcopy /s /e /y ..\Midas\src\builtins ..\release-windows\builtins
 echo d | xcopy /s /e /y ..\Midas\vstl ..\release-windows\vstl
-copy ..\Midas\valec.py ..\release-windows\valec.py
 copy releaseREADME.txt ..\release-windows\README.txt
 copy valec-help* ..\release-windows
 copy ..\Midas\build\Debug\midas.exe ..\release-windows\midas.exe
 copy ..\Midas\LLVM-C.dll ..\release-windows\LLVM-C.dll
 git clone https://github.com/ValeLang/stdlib ../release-windows/stdlib
 echo d | xcopy /s /e /y helloworld ..\release-windows\samples\helloworld
+cp ..\Driver\build\valec ..\release-windows\valec

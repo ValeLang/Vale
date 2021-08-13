bd53e20f0359431d52e8db834aa0d64fd7c5425a
Evan Ovadia <verdagon@gmail.com>
2021-08-12 17:43:35 -0700

    scripts

--- build-compiler-mac.sh
+++ build-compiler-mac.sh
@@ -1,27 +1,31 @@
 # This script builds the Vale compiler, runs some tests on it, and also packages up a release zip file.
 # It assumes we've already ran install-compiler-prereqs-mac.sh, or otherwise installed all the dependencies.
 
+git clone --single-branch ${1:-https://github.com/ValeLang/stdlib} --branch ${2:-master}
+
 cd Valestrom
 
-sbt assembly
+sbt assembly || { echo 'Valestrom build failed' ; exit 1; }
 
 cd ../Midas
 
 LLVM_CMAKE_DIR="/usr/local/Cellar/llvm@11/`ls /usr/local/Cellar/llvm@11`/lib/cmake/llvm"
 
-cmake -B build -D LLVM_DIR="$LLVM_CMAKE_DIR"
+cmake -B build -D LLVM_DIR="$LLVM_CMAKE_DIR" || { echo 'Midas build failed' ; exit 1; }
 
 cd build
 
 make
 
-cd ..
+cd ../../Driver
 
-python3 -m unittest -f -k assist
-if [ ! $? -eq 0 ]; then
-  echo "Running tests failed, aborting!"
-  exit 1
-fi
+call build.bat
+
+cd ../Tester
+
+call build.bat
+
+build/tester build --valestrom_dir_override ../Valestrom --midas_dir_override ../Midas/build --builtins_dir_override ../Midas/src/builtins --valec_dir_override ../Driver/build --midas_tests_dir ../Midas/test --concurrent 6 @assist
 
 cd ../scripts
 

a2240b39b85c3d0723d6858de40ba512ac73f94b
Evan Ovadia <verdagon@gmail.com>
2021-08-13 13:10:12 -0700

    Bootstrapping fixes

--- build-compiler-mac.sh
+++ build-compiler-mac.sh
@@ -1,7 +1,12 @@
 # This script builds the Vale compiler, runs some tests on it, and also packages up a release zip file.
 # It assumes we've already ran install-compiler-prereqs-mac.sh, or otherwise installed all the dependencies.
 
-git clone --single-branch ${1:-https://github.com/ValeLang/stdlib} --branch ${2:-master}
+BOOTSTRAPPING_VALEC_DIR="$1"
+if [ "$BOOTSTRAPPING_VALEC_DIR" == "" ]; then
+  echo "Please supply the bootstrapping valec directory."
+  echo "Example: ~/ValeCompiler-0.1.3.3-Ubuntu"
+  exit
+fi
 
 cd Valestrom
 
@@ -22,15 +27,15 @@ make
 cd ../../Driver
 
 echo Compiling Driver...
-./build-mac.sh || { echo 'Driver build failed, aborting.' ; exit 1; }
+./build-mac.sh $BOOTSTRAPPING_VALEC_DIR || { echo 'Driver build failed, aborting.' ; exit 1; }
 
 cd ../Tester
 
 echo Compiling Tester...
-./build.sh || { echo 'Tester build failed, aborting.' ; exit 1; }
+./build.sh $BOOTSTRAPPING_VALEC_DIR || { echo 'Tester build failed, aborting.' ; exit 1; }
 
 echo Running Tester...
-build/tester --valestrom_dir_override ../Valestrom --midas_dir_override ../Midas/build --builtins_dir_override ../Midas/src/builtins --valec_dir_override ../Driver/build --midas_tests_dir ../Midas/test --concurrent 6 @assist || { echo 'Tests failed, aborting.' ; exit 1; }
+build/testvalec --valestrom_dir_override ../Valestrom --midas_dir_override ../Midas/build --builtins_dir_override ../Midas/src/builtins --valec_dir_override ../Driver/build --midas_tests_dir ../Midas/test --concurrent 6 @assist || { echo 'Tests failed, aborting.' ; exit 1; }
 
 cd ../scripts
 
--- install-compiler-prereqs-linux.sh
+++ install-compiler-prereqs-linux.sh
@@ -2,13 +2,27 @@
 
 # WARNING: There are sudo commands below! Review all sudo commands before running them.
 
-if [ "$1" == "" ]; then
+LLVM_DIR="$1"
+if [ "$LLVM_DIR" == "" ]; then
   echo "First arg must be path to unzip LLVM to."
+  echo "Example: ~/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10"
+  exit
 fi
 
-if [ "$2" == "" ]; then
-  echo "Second arg must be path to unzip a bootstrapping stable Vale compiler to."
+LLVM_CMAKE_DIR="$LLVM_DIR/lib/cmake/llvm"
+if [ ! -d "$LLVM_CMAKE_DIR" ]; then
+  echo "Directory not found: $LLVM_CMAKE_DIR"
+  echo "Are you sure you specified the right LLVM directory?"
+  exit
 fi
+
+BOOTSTRAPPING_VALEC_DIR="$2"
+if [ "$BOOTSTRAPPING_VALEC_DIR" == "" ]; then
+  echo "Second arg must be path to unzip a bootstrapping stable Vale compiler to."
+  echo "Example: ~/ValeCompiler-0.1.3.3-Ubuntu"
+  exit
+fi
+
 
 # Install misc dependencies
 echo "Installing dependencies..."
@@ -29,16 +43,16 @@ curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89
 sudo apt update
 sudo apt install -y sbt
 
-echo "Downloading and unzipping stable bootstrapping valec to $2..."
+echo "Downloading and unzipping stable bootstrapping valec to $BOOTSTRAPPING_VALEC_DIR..."
 # Install stable valec, for the .vale parts of the compiler
 curl -L https://vale.dev/releases/ValeCompiler-0.1.3.3-Ubuntu.zip
-unzip ValeCompiler-0.1.3.3-Ubuntu.zip -d $2
+unzip ValeCompiler-0.1.3.3-Ubuntu.zip -d $BOOTSTRAPPING_VALEC_DIR
 # Doesnt work, see https://github.com/ValeLang/Vale/issues/306
 # echo 'export PATH=$PATH:~/ValeCompiler-0.1.3.3-Ubuntu' >> ~/.bashrc
 
-echo "Downloading and unzipping LLVM to $1..."
+echo "Downloading and unzipping LLVM to $LLVM_DIR..."
 # Install LLVM 11.1.0 (from https://github.com/llvm/llvm-project/releases/tag/llvmorg-11.1.0)
 curl -L https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz --output /tmp/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz
-mkdir -p $1
-tar xf /tmp/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz -C $1
+mkdir -p $LLVM_DIR
+tar xf /tmp/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz -C $LLVM_DIR
 # Later, we'll need to feed this to a cmake command so it knows where the LLVM libraries are.

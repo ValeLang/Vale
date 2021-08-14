e54204a5a13c453e2e5ccdd7b475384887e07939
Evan Ovadia <verdagon@gmail.com>
2021-08-13 19:57:00 -0700

    Using supplied bootstrapping valec+stdlib for Driver and Tester now Cleaned out old scripts Clarified flags for Driver Made the scripts test the unzipped zip Made the scripts return 1 if they fail Removed valec.py from zip Split windows script into an install and build script, like mac and linux

--- install-compiler-prereqs-mac.sh
+++ install-compiler-prereqs-mac.sh
@@ -2,22 +2,38 @@
 
 # WARNING: There are sudo commands below! Review all sudo commands before running them.
 
+BOOTSTRAPPING_VALEC_DIR="$1"
+if [ "$BOOTSTRAPPING_VALEC_DIR" == "" ]; then
+  echo "First arg must be path to unzip a bootstrapping stable Vale compiler to."
+  echo "Example: ~/ValeCompiler-0.1.3.3-Ubuntu"
+  exit
+fi
+
 # Installs Xcode if you haven't already.
+echo Installing Xcode...
 xcode-select --install
 # (press ok)
 
 # Install Java.
+echo Installing Java...
 curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jre_x64_mac_hotspot_11.0.10_9.tar.gz --output adoptopenjdk.tar.gz
 tar xzf adoptopenjdk.tar.gz
 # Add vars to .zshrc (or .bashrc)
 echo 'export PATH=~/jdk-11.0.10+9-jre/Contents/Home/bin:$PATH' >> ~/.zshrc
 
 # Installs brew, like said on https://brew.sh/
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# (press return)
+echo Installing brew...
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+# See https://stackoverflow.com/questions/25535407/bypassing-prompt-to-press-return-in-homebrew-install-script
+# for why we do </dev/null
+
+# Install stable valec, for the .vale parts of the compiler
+echo "Downloading and unzipping stable bootstrapping valec to $BOOTSTRAPPING_VALEC_DIR..."
+curl -L https://vale.dev/releases/ValeCompiler-0.1.3.3-Mac.zip --output /tmp/ValeCompiler-0.1.1.3-Mac.zip
+unzip /tmp/ValeCompiler-0.1.1.3-Mac.zip -d $BOOTSTRAPPING_VALEC_DIR
+echo 'export PATH=$PATH:~/ValeCompiler-0.1.3.3-Mac' >> ~/.zshrc
 
 # Install misc dependencies
+echo "Downloading and unzipping depdendencies and LLVM..."
 brew install llvm@11 sbt cmake
-echo 'export PATH=/usr/local/Cellar/llvm@11/11.1.0_2/bin:$PATH' >> ~/.zshrc
-
-source ~/.zshrc
+echo 'export PATH=$PATH:/usr/local/Cellar/llvm@11/11.1.0_2/bin' >> ~/.zshrc
--- scripts/package-windows.bat
+++ scripts/package-windows.bat
@@ -5,11 +5,17 @@ copy ..\Valestrom\Valestrom.jar ..\release-windows\Valestrom.jar
 
 echo d | xcopy /s /e /y ..\Valestrom\Tests\test\main\resources\programs ..\release-windows\samples
 echo d | xcopy /s /e /y ..\Midas\src\builtins ..\release-windows\builtins
-echo d | xcopy /s /e /y ..\Midas\vstl ..\release-windows\vstl
-copy ..\Midas\valec.py ..\release-windows\valec.py
 copy releaseREADME.txt ..\release-windows\README.txt
-copy valec-help* ..\release-windows
+copy valec* ..\release-windows
 copy ..\Midas\build\Debug\midas.exe ..\release-windows\midas.exe
-copy ..\Midas\LLVM-C.dll ..\release-windows\LLVM-C.dll
 git clone https://github.com/ValeLang/stdlib ../release-windows/stdlib
 echo d | xcopy /s /e /y helloworld ..\release-windows\samples\helloworld
+copy ..\Driver\build\valec.exe ..\release-windows\valec.exe
+
+copy %1\bin\LLVM-C.dll ..\release-windows\LLVM-C.dll
+
+cd ..\release-windows
+
+PATH=%PATH%;C:\Program Files\7-Zip
+PATH=%PATH%;C:\Program Files\7-Zip\7z.exe
+7z a ValeCompiler.zip *

324c31cb00d9c5ee271bbc433ea98cd7660f37a3
Evan Ovadia <verdagon@gmail.com>
2021-08-13 19:57:00 -0700

    Using supplied bootstrapping valec+stdlib for Driver and Tester now Cleaned out old scripts Clarified flags for Driver Made the scripts test the unzipped zip Made the scripts return 1 if they fail Removed valec.py from zip Split windows script into an install and build script, like mac and linux

--- Tester/build.bat
+++ Tester/build.bat
@@ -1,2 +1,2 @@
-rem Tester build.bat
-..\Driver\build\valec build tester=src stdlib=..\stdlib\src --output_dir build -o tester --builtins_dir_override ..\Midas\src\builtins --valestrom_dir_override ..\Valestrom --midas_dir_override ..\Midas\build\Debug
+
+%1\valec.exe build tester=src stdlib=%1\stdlib\src --output_dir build -o testvalec

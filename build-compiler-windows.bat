90bf73125ef7a7c48e6f0120636e43a99ab0470b
Evan Ovadia <verdagon@gmail.com>
2021-08-13 19:57:00 -0700

    Using supplied bootstrapping valec+stdlib for Driver and Tester now Cleaned out old scripts Clarified flags for Driver Made the scripts test the unzipped zip Made the scripts return 1 if they fail Removed valec.py from zip Split windows script into an install and build script, like mac and linux

---
+++ build-compiler-windows.bat
@@ -0,0 +1,46 @@
+
+cd Valestrom
+
+echo Compiling Valestrom...
+call sbt assembly || echo "Valestrom build failed, aborting." && exit /b 1
+
+cd ..
+
+cd Midas
+
+echo Generating Midas...
+cmake -B build -D LLVM_DIR="%1\lib\cmake\llvm" || echo "Midas generate failed, aborting." && exit /b 1
+
+cd build
+
+echo Compiling Midas...
+cmake --build . || echo "Midas build failed, aborting." && exit /b 1
+
+
+
+
+cd ..\..\Driver
+
+echo Compiling Driver...
+call build.bat %2 || echo "Driver build failed, aborting." && exit /b 1
+
+
+
+cd ..\scripts
+
+call package-windows.bat
+
+
+
+cd ..\Tester
+
+
+mkdir .\BuiltValeCompiler
+tar -xf ..\release-windows\ValeCompiler.zip -C .\BuiltValeCompiler
+
+
+echo Compiling Tester...
+call build.bat %2 || echo "Tester build failed, aborting." && exit /b 1
+
+echo Running Tester...
+build\testvalec --valestrom_path .\BuiltValeCompiler\Valestrom.jar --midas_path .\BuiltValeCompiler\midas.exe --builtins_dir .\BuiltValeCompiler\builtins --valec_path .\BuiltValeCompiler\valec.exe --midas_tests_dir ..\Midas\test --valestrom_tests_dir ..\Valestrom --concurrent 6 @assist || echo "Tests failed, aborting." && exit /b 1
---
+++ install-compiler-prereqs-windows.bat
@@ -0,0 +1,8 @@
+
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://firebasestorage.googleapis.com/v0/b/valesite.appspot.com/o/llvm-install-minimal.zip?alt=media&token=7e73117d-5319-4a78-bfba-04f8d3b91060' -OutFile '%temp%\llvm-install-minimal.zip'"
+mkdir %1
+tar xf "%temp%\llvm-install-minimal.zip" -C %1
+
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://vale.dev/releases/ValeCompiler-0.1.3.4-Win.zip' -OutFile '%temp%\ValeCompiler-0.1.3.4-Win.zip'"
+mkdir %2
+tar xf "%temp%\ValeCompiler-0.1.3.4-Win.zip" -C %2

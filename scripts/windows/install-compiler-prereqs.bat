6e6473101befc0e03265332db8d7f43fac301bca
Evan Ovadia <verdagon@gmail.com>
2022-05-11 22:19:42 -0400

    Bumped version

--- scripts/windows/install-compiler-prereqs.bat
+++ scripts/windows/install-compiler-prereqs.bat
@@ -7,6 +7,6 @@ tar xf "%temp%\llvm-install-minimal.zip" -C %1
 
 echo Downloading bootstrapping Vale compiler...
 
-powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/ValeLang/Vale/releases/download/v0.2.0/Vale-Windows-0.2.0.10.zip' -OutFile '%temp%\BootstrappingValeCompiler.zip'"
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/ValeLang/Vale/releases/download/v0.2.0/Vale-Windows-0.2.0.11.zip' -OutFile '%temp%\BootstrappingValeCompiler.zip'"
 mkdir %2
 tar xf "%temp%\BootstrappingValeCompiler.zip" -C %2

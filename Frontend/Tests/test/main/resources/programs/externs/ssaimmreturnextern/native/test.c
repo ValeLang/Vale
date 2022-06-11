0afb7e69de15717510df8830def44961bc3cf759
Evan Ovadia <verdagon@gmail.com>
2022-06-11 18:19:26 -0400

    Enabled more tests for replayability

--- Frontend/Tests/test/main/resources/programs/externs/ssaimmreturnextern/native/test.c
+++ Frontend/Tests/test/main/resources/programs/externs/ssaimmreturnextern/native/test.c
@@ -5,11 +5,16 @@
 #include "vtest/ImmIntArray.h"
 #include "vtest/cMakeRSA.h"
 
+// We use incrementIntFile to get some side effects to test replayability, see AASETR.
+int64_t incrementIntFile(const char* filename);
+
 vtest_ImmIntArray* vtest_cMakeRSA() {
+  int runNumber = incrementIntFile("myfile.bin");
+
   assert(vtest_ImmIntArray_SIZE == 3);
   vtest_ImmIntArray* arr = malloc(sizeof(vtest_ImmIntArray) + sizeof(ValeInt) * vtest_ImmIntArray_SIZE);
-  arr->elements[0] = 13;
-  arr->elements[1] = 14;
-  arr->elements[2] = 15;
+  arr->elements[0] = 13 * runNumber;
+  arr->elements[1] = 14 * runNumber;
+  arr->elements[2] = 15 * runNumber;
   return arr;
 }

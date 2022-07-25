4ec7429f387a1fc7b3af28417168921feebba861
Zodey <balint.mate.2006@gmail.com>
2022-07-25 17:48:14 +0200

    added random support for multiple types

--- stdlib/src/random/native/random.c
+++ stdlib/src/random/native/random.c
@@ -1,13 +1,31 @@
 #include <time.h>
+#include <math.h>
 #include <stdlib.h>
 
 #include "stdlib/randomize.h"
-#include "stdlib/random.h"
+#include "stdlib/randomBool.h"
+#include "stdlib/randomInt.h"
+#include "stdlib/randomI64.h"
+#include "stdlib/randomFloat.h"
 
 void stdlib_randomize(void) {
    srand(time(0));
 }
 
-int32_t stdlib_random(int32_t lower, int32_t upper) {
+int8_t stdlib_randomBool() {
+  return (rand() & 1);
+}
+
+int32_t stdlib_randomInt(int32_t lower, int32_t upper) {
   return ((rand() % (upper + 1 - lower)) + lower);
+}
+
+int64_t stdlib_randomI64(int64_t lower, int64_t upper) {
+  return ((rand() % (upper + 1 - lower)) + lower);
+}
+
+double stdlib_randomFloat(double lower, double upper) {
+  double random = ((double) rand()) / (double) RAND_MAX;
+  float range = upper - lower;  
+  return (random*range) + lower;
 }

714e302f9812b1afc01c6c876995700e14abcc6e
Evan Ovadia <verdagon@gmail.com>
2023-04-07 22:45:41 -0600

    Fixed replayability for strings

--- Frontend/Tests/test/main/resources/programs/externs/strlenextern/native/test.c
+++ Frontend/Tests/test/main/resources/programs/externs/strlenextern/native/test.c
@@ -2,8 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 
+// We use incrementIntFile to get some side effects to test replayability, see AASETR.
+int64_t incrementIntFile(const char* filename);
+
 ValeInt vtest_extStrLen(ValeStr* haystackContainerStr) {
+  int runNumber = incrementIntFile("myfile.bin");
+
+  printf("extstrlen run number %d\n", runNumber);
+
   char* haystackContainerChars = haystackContainerStr->chars;
   ValeInt result = strlen(haystackContainerChars);
-  return result;
+  return result * runNumber;
 }
--- Frontend/Tests/test/main/resources/programs/externs/strreturnexport/native/test.c
+++ Frontend/Tests/test/main/resources/programs/externs/strreturnexport/native/test.c
@@ -4,14 +4,23 @@
 #include <assert.h>
 #include "vtest/getAStr.h"
 
+// We use incrementIntFile to get some side effects to test replayability, see AASETR.
+int64_t incrementIntFile(const char* filename);
+
 ValeStr* vtest_runExtCommand() {
+  int runNumber = incrementIntFile("myfile.bin");
+
   ValeStr* str = vtest_getAStr();
 
   assert(str->length == 6);
   int diff = strncmp(str->chars, "hello!", 6);
   assert(diff == 0);
 
-  ValeStr* result = ValeStrFrom(str->chars);
+  // "hello!", "hello!hello!", etc.
+  ValeStr* result = ValeStrNew(str->length * runNumber);
+  for (int i = 0; i < runNumber; i++) {
+    strncpy(result->chars + strlen("hello!") * runNumber, "hello!", strlen("hello!"));
+  }
 
   free(str);
 

ae0ede3de7c98063538dd52033ea4c461651d7ba
Evan Ovadia <verdagon@evans-mbp.lan>
2023-04-20 16:35:37 -0600

    Updating jdk source

--- scripts/ubuntu/install-compiler-prereqs.sh
+++ scripts/ubuntu/install-compiler-prereqs.sh
@@ -78,12 +78,12 @@ fi
 # Install Java
 if [[ $INSTALL_JAVA != 0 ]]; then
   echo -e "\n${TEXT_GREEN}Installing Java...${TEXT_RESET}"
-  sudo apt-get install -y wget apt-transport-https gnupg
-  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -
-  echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-
+  sudo apt install -y wget apt-transport-https
+  mkdir -p /etc/apt/keyrings
+  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+  echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
   sudo apt update
-  sudo apt install temurin-11-jdk 
+  sudo apt install -y temurin-11-jdk
 fi
 
 # Install SBT

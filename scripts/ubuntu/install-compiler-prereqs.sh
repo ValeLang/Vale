3a42c690254c84fa259548b8862f29a8e90cd8b9
Evan Ovadia <verdagon@evans-mbp.lan>
2023-04-20 16:44:38 -0600

    Updating sbt sources

--- scripts/ubuntu/install-compiler-prereqs.sh
+++ scripts/ubuntu/install-compiler-prereqs.sh
@@ -89,11 +89,14 @@ fi
 # Install SBT
 if [[ $INSTALL_SBT != 0 ]]; then
   echo -e "\n${TEXT_GREEN}Installing sbt...${TEXT_RESET}"
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https curl gnupg -yqq
   echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
   echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
-  curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-  sudo apt update
-  sudo apt install -y sbt
+  curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+  sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+  sudo apt-get update
+  sudo apt-get install -y sbt
 fi
 
 # Install bootstrap compiler

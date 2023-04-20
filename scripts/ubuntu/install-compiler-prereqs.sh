50bb3b8e88389b2e11770dd91ecad795a0e0cef2
Evan Ovadia <192751+Verdagon@users.noreply.github.com>
2023-04-20 01:45:36 -0400

    Updated JDK source

--- scripts/ubuntu/install-compiler-prereqs.sh
+++ scripts/ubuntu/install-compiler-prereqs.sh
@@ -79,9 +79,9 @@ fi
 if [[ $INSTALL_JAVA != 0 ]]; then
   echo -e "\n${TEXT_GREEN}Installing Java...${TEXT_RESET}"
   apt-get install -y wget apt-transport-https gnupg
-  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add -
-  echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-
+  mkdir -p /etc/apt/keyrings
+  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+  echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
   sudo apt update
   sudo apt install temurin-11-jdk 
 fi

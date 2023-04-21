9e80381d89577d01cb2bff5c5d4b7291108a5404
Evan Ovadia <verdagon@evans-mbp.lan>
2023-04-21 14:40:11 -0600

    Fix for CI

--- scripts/ubuntu/install-compiler-prereqs.sh
+++ scripts/ubuntu/install-compiler-prereqs.sh
@@ -80,7 +80,7 @@ if [[ $INSTALL_JAVA != 0 ]]; then
   echo -e "\n${TEXT_GREEN}Installing Java...${TEXT_RESET}"
   sudo apt-get install -y wget apt-transport-https gnupg
   mkdir -p /etc/apt/keyrings
-  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+  wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
   echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
   sudo apt update
   sudo apt install -y temurin-11-jdk

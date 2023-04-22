cd8de4046b75e7caa11a283e24c774ee5b6c7b49
Evan Ovadia <verdagon@Evans-MacBook-Pro.local>
2023-04-21 20:31:29 -0600

    Adding docker to CI

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,9 +1,4 @@
 
-echo git pull origin $VALE_BRANCH
-git pull origin $VALE_BRANCH
+set -euo pipefail
 
-echo ./scripts/ubuntu/build-compiler.sh /Outer/LLVMForVale/clang+llvm-13.0.1-x86_64-linux-gnu-ubuntu-18.04 /Outer/BootstrappingValeCompiler --test=all ./scripts/VERSION
-./scripts/ubuntu/build-compiler.sh /Outer/LLVMForVale/clang+llvm-13.0.1-x86_64-linux-gnu-ubuntu-18.04 /Outer/BootstrappingValeCompiler --test=all ./scripts/VERSION
-
-echo Success!
-echo Vale compiler can be found at: /Outer/Vale/release-ubuntu/Vale-Ubuntu-0.zip
+/Vale/release-ubuntu/valec $@

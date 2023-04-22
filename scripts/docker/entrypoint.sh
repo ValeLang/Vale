217ca1ed4bd586b72a30e6d71fb7ee202cf0ec1c
Evan Ovadia <>
2023-04-21 23:11:25 -0600

    Merged

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,7 +1,9 @@
 
-git pull origin $0
+echo git pull origin $VALE_BRANCH
+git pull origin $VALE_BRANCH
 
+echo ./scripts/ubuntu/build-compiler.sh /Outer/LLVMForVale/clang+llvm-13.0.1-x86_64-linux-gnu-ubuntu-18.04 /Outer/BootstrappingValeCompiler --test=all ./scripts/VERSION
 ./scripts/ubuntu/build-compiler.sh /Outer/LLVMForVale/clang+llvm-13.0.1-x86_64-linux-gnu-ubuntu-18.04 /Outer/BootstrappingValeCompiler --test=all ./scripts/VERSION
 
 echo Success!
-echo Vale compiler can be found at /Outer/Vale/release-ubuntu/Vale-Ubuntu-0.zip
+echo Vale compiler can be found at: /Outer/Vale/release-ubuntu/Vale-Ubuntu-0.zip

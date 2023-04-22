f5e5a34b397da0a1f21d268c95ca17d0b39f5d1a
Evan Ovadia <>
2023-04-21 23:11:25 -0600

    Merged

--- scripts/ubuntu/install-compiler-prereqs.sh
+++ scripts/ubuntu/install-compiler-prereqs.sh
@@ -81,7 +81,7 @@ if [[ $INSTALL_JAVA != 0 ]]; then
   sudo apt-get install -y wget apt-transport-https gnupg
   mkdir -p /etc/apt/keyrings
   wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
-  echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+  echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
   sudo apt-get update
   sudo apt-get install -y temurin-11-jdk
 fi

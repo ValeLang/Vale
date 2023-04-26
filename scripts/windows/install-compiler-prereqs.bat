275cc0914d1802972937e1b19b647fd235a2a083
Evan Ovadia <verdagon@gmail.com>
2023-04-26 16:59:25 -0400

    Windows CI fixes

--- scripts/windows/install-compiler-prereqs.bat
+++ scripts/windows/install-compiler-prereqs.bat
@@ -1,13 +1,13 @@
 
 echo Downloading minimal LLVM...
 
-powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/LLVM-16.0.0-win64.exe' -OutFile '%temp%\LLVM-16.0.0-win64.exe'"
-Rem See https://stackoverflow.com/questions/16087674/how-can-i-make-my-nsis-silent-installer-block-until-complete
-Rem This command might hang indefinitely if there's a problem.
-start "" /WAIT runas.exe /savecred /user:administrator "'%temp%\LLVM-16.0.0-win64.exe' /S"
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/Verdagon/LLVMWinMinimal/releases/download/v1.3/LLVM16.zip' -OutFile '%temp%\LLVM16.zip'"
+mkdir %1
+tar xf "%temp%\LLVM16.zip" -C %1
+dir %1
 
 echo Downloading bootstrapping Vale compiler...
 
-powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/ValeLang/Vale/releases/download/v0.2.0/Vale-Windows-0.2.0.26.zip' -OutFile '%temp%\BootstrappingValeCompiler.zip'"
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/ValeLang/Vale/releases/download/v0.2.0/Vale-Windows-0.2.0.23.zip' -OutFile '%temp%\BootstrappingValeCompiler.zip'"
 mkdir %2
 tar xf "%temp%\BootstrappingValeCompiler.zip" -C %2

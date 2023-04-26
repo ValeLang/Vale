118d2627fcf106763a280255903280ca41f25d97
Evan Ovadia <verdagon@gmail.com>
2023-04-26 16:34:31 -0400

    Windows CI fixes

--- scripts/windows/install-compiler-prereqs.bat
+++ scripts/windows/install-compiler-prereqs.bat
@@ -4,7 +4,8 @@ echo Downloading minimal LLVM...
 powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/LLVM-16.0.0-win64.exe' -OutFile '%temp%\LLVM-16.0.0-win64.exe'"
 mkdir %1
 Rem See https://stackoverflow.com/questions/16087674/how-can-i-make-my-nsis-silent-installer-block-until-complete
-start "" /WAIT '%temp%\LLVM-16.0.0-win64.exe' /S /D "%1"
+Rem This command might hang indefinitely if there's a problem.
+start "" /WAIT runas.exe /savecred /user:administrator "'%temp%\LLVM-16.0.0-win64.exe' /S /D \"%1\""
 dir %1
 
 echo Downloading bootstrapping Vale compiler...

13061e958402469fa29ceedf0397bd7989bb4cbf
Evan Ovadia <>
2023-04-26 23:32:20 -0600

    Windows fixes

--- scripts/windows/install-compiler-prereqs.bat
+++ scripts/windows/install-compiler-prereqs.bat
@@ -2,8 +2,8 @@
 echo Downloading minimal LLVM...
 powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/llvm-project-16.0.0.src.tar.xz' -OutFile '%temp%\llvm-project-16.0.0.src.tar.xz'"
 mkdir "%temp%\LLVM"
-cd "%temp%\LLVM"
 tar xf "%temp%\llvm-project-16.0.0.src.tar.xz" -C "%temp%\LLVM" && exit /b 1
+mkdir %1 && exit /b 1
 dir %1 && exit /b 1
 cmake "%temp%\LLVM" -G "Visual Studio 17 2022" -Thost=x64 -A x64 -D "CMAKE_INSTALL_PREFIX=%temp%\LLVM" -D CMAKE_BUILD_TYPE=MinSizeRel -D LLVM_TARGETS_TO_BUILD="X86;WebAssembly" -DCMAKE_INSTALL_PREFIX=%1 && exit /b 1
 cmake --build . --target install && exit /b 1

80f9c536e0fbadc106ad3f52fc43c673caf11803
Evan Ovadia <>
2023-07-13 17:09:02 -0400

    Regions merges: callLocation, renames, imports

--- Frontend/ParsingPass/src/dev/vale/parsing/StringParserTests.scala
+++ Frontend/ParsingPass/src/dev/vale/parsing/StringParserTests.scala
@@ -4,6 +4,7 @@ import dev.vale.lexing.{Lexer, LexingIterator}
 import dev.vale.parsing.ast._
 import dev.vale.{Collector, Interner, Keywords, StrI, vimpl}
 import dev.vale.parsing.ast.{ConstantIntPE, ConstantStrPE, FunctionCallPE, LookupNameP, LookupPE, NameP, StrInterpolatePE}
+import org.scalatest.FunSuite
 import org.scalatest.Matchers.convertToAnyShouldWrapper
 import org.scalatest._
 import org.scalatest._
